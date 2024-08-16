2c740184452172c797eb1b5cf47841c4437b7884
河田鷹介 <benefium@gmail.com>
2024-08-17 05:19:48 +0900

    つくりかけpush

--- app/src/components/palette/BasicPalette.tsx
+++ app/src/components/palette/BasicPalette.tsx
@@ -1,6 +1,6 @@
 import { Button, Popover, PopoverButton, PopoverPanel } from "@headlessui/react"
 import { InlineIcon } from "../InlineIcon"
-import { FaShapes } from "react-icons/fa"
+import { FaRegCircle, FaShapes } from "react-icons/fa"
 import { SlMagnifier } from "react-icons/sl"
 import { ColorTheme, useColorTheme } from "../../stores/theme"
 import { MdOutlineDarkMode, MdOutlineLightbulb, MdOutlineLightMode } from "react-icons/md"
@@ -8,6 +8,8 @@ import { ScaleView } from "../../views/GraphView/ScaleView"
 import { Vector } from "../../types"
 import { ComponentWithProps } from "../../types/components"
 import { ThemeSelector } from "../ThemeSelector"
+import { MultipleButtons } from "../MultipleButtons"
+import { FaRegSquareFull } from "react-icons/fa6"
 
 const getIconTypeForColorTheme = (colorTheme: ColorTheme) => {
   switch (colorTheme) {
@@ -33,14 +35,42 @@ export const BasicPalette: ComponentWithProps<{
   return <div className="basic-palette">
     <div className="grid grid-cols-1 grid-flow-row gap-2 p-1">
       <div>
-        <Button className="basic-palette-button p-1">
-          <InlineIcon i={<FaShapes className="w-6 h-6" />} />
-        </Button>
+        <Popover className="relative">
+          <PopoverButton as="div">
+            <Button className="basic-palette-button p-1">
+              <InlineIcon i={<FaShapes className="w-6 h-6" />} />
+            </Button>
+          </PopoverButton>
+          <PopoverPanel anchor="right" transition className="flex flex-col transition duration-200 ease-out data-[closed]:-translate-x-1 data-[closed]:opacity-0">
+            <MultipleButtons
+              items={[
+                {
+                  key: "Rectangle",
+                  content: <div
+
+                  >
+                    <InlineIcon i={<FaRegSquareFull className="w-6 h-6" />} />
+                  </div>
+                },
+                {
+                  key: "Circle",
+                  content: <div
+                  >
+                    <InlineIcon i={<FaRegCircle className="w-6 h-6" />} />
+                  </div>
+                },
+              ]}
+              onClick={(item) => {
+                console.log(item);
+              }}
+            />
+          </PopoverPanel>
+        </Popover>
       </div>
 
       <div>
         <Popover className="relative">
-          <PopoverButton>
+          <PopoverButton as="div">
             <Button className="basic-palette-button p-1">
               <InlineIcon i={<SlMagnifier className="w-6 h-6" />} />
             </Button>
@@ -53,7 +83,7 @@ export const BasicPalette: ComponentWithProps<{
 
       <div>
         <Popover className="relative">
-          <PopoverButton>
+          <PopoverButton as="div">
             <Button className="basic-palette-button p-1">
               <InlineIcon i={<IconTypeForColorTheme className="w-6 h-6" />} />
             </Button>
